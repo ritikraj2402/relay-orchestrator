9a29762c16b92809a2d82d020d26388cdeef3beb
Ritik Raj <ritikrajvi@gmail.com>
2026-06-02 17:45:38 +0530

    Added spring boot project setup

---
+++ src/main/java/com/ritik/relayorchestrator/RelayOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.ritik.relayorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ritik/relayorchestrator/RelayOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritik.relayorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
